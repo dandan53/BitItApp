f94ff34b1e3fab97cd6463b647a77efcb2d946a8
dandan53 <dandan53@gmail.com>
2015-03-09 17:30:40 +0200

    root

--- BitItApp/App_Start/RouteConfig.cs
+++ BitItApp/App_Start/RouteConfig.cs
@@ -11,6 +11,8 @@ namespace BitItApp
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.IgnoreRoute("");
+            
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
